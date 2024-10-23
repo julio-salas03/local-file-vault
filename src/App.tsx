dcac1e3b47a2bc6ee1df25b0fe6b34f30d6d5a32
Julio Salas <84051776+julio-salas03@users.noreply.github.com>
2024-10-23 12:57:03 -0500

    add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import type { Component } from 'solid-js';
+import { Button } from '@/components/ui/button';
 
 const App: Component = () => {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
+    <div>
+      <Button>click me</Button>
+    </div>
   );
 };
 
