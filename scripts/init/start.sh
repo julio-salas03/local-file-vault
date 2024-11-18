a6ddb87c5ec44af7c983bf7a08468e25f2679f36
Julio Salas <84051776+julio-salas03@users.noreply.github.com>
2024-11-18 14:08:41 -0500

    generate JWT_SECRET on init script

--- scripts/init/start.sh
+++ scripts/init/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp .env.example .env
+
 CONTAINER_NAME="local-file-vault-db"
 
 # Start the Docker container
@@ -31,3 +33,14 @@ bun install --frozen-lockfile
 # Build and start the project
 bun run build
 nohup bun run start > /dev/null &
+
+# Shell colors
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+RESET='\033[0m'
+
+# Generate basic JWT_SECRET
+JWT_SECRET=$(openssl rand -base64 32)
+
+echo -e "${CYAN}Copy this key (${GREEN}$JWT_SECRET${CYAN}) and paste it in the '${YELLOW}JWT_SECRET${CYAN}' key on your '${YELLOW}.env${CYAN}' file"
